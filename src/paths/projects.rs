68a31557548aca4bb43a6d865ab2382a8ffb9f63
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2022-01-11 02:14:57 +0900

    added dev mode

--- src/paths/projects.rs
+++ src/paths/projects.rs
@@ -1,5 +1,8 @@
 use async_std::path::PathBuf;
 
+#[cfg(debug_assertions)]
+const PACKAGE_NAME: &str = concat!(env!("CARGO_PKG_NAME"), "_dev");
+#[cfg(not(debug_assertions))]
 const PACKAGE_NAME: &str = env!("CARGO_PKG_NAME");
 
 #[allow(dead_code)]
