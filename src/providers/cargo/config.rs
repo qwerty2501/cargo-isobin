12186f9f924ea5914b9d5c52c4c1e85ea5f655be
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2023-10-08 04:46:51 +0900

    removed cargo_toml

--- src/providers/cargo/config.rs
+++ src/providers/cargo/config.rs
@@ -1,10 +1,49 @@
 use std::collections::HashMap;
 
-pub use cargo_toml::Dependency as CargoInstallDependency;
-pub use cargo_toml::DependencyDetail as CargoInstallDependencyDetail;
 use serde_derive::{Deserialize, Serialize};
 #[derive(Deserialize, Serialize, Debug, PartialEq, new, Default, Getters)]
 pub struct CargoConfig {
     #[serde(serialize_with = "toml::ser::tables_last")]
     installs: HashMap<String, CargoInstallDependency>,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+#[allow(clippy::large_enum_variant)]
+pub enum CargoInstallDependency {
+    Simple(String),
+    Detailed(CargoInstallDependencyDetail),
+}
+impl CargoInstallDependency {
+    pub fn into_detail(self) -> CargoInstallDependencyDetail {
+        match self {
+            Self::Simple(version) => CargoInstallDependencyDetail {
+                version: Some(version),
+                ..Default::default()
+            },
+            Self::Detailed(detail) => detail,
+        }
+    }
+}
+
+#[allow(clippy::too_many_arguments)]
+#[derive(Debug, Clone, PartialEq, Default, Serialize, Deserialize, Getters)]
+pub struct CargoInstallDependencyDetail {
+    #[serde(default)]
+    pub bins: Vec<String>,
+    pub version: Option<String>,
+    pub registry: Option<String>,
+    #[serde(alias = "registry-index", alias = "registryIndex")]
+    pub registry_index: Option<String>,
+    pub path: Option<String>,
+    pub git: Option<String>,
+    pub branch: Option<String>,
+    pub tag: Option<String>,
+    pub rev: Option<String>,
+    #[serde(default)]
+    pub features: Vec<String>,
+    #[serde(default)]
+    pub optional: bool,
+    #[serde(alias = "default-features", alias = "defaultFeatures")]
+    pub default_features: Option<bool>,
 }
